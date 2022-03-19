c7204575f6f77a3fd3b8a02550f7d4802749e3f9
Dileep Reddy <dileepreddyp7@gmail.com>
2022-03-19 11:54:32 -0400

    cat model works?

--- lib/Drawzone.jsx
+++ lib/Drawzone.jsx
@@ -1,356 +1,190 @@
-// // Copyright (c) 2019 ml5
-// //
-// // This software is released under the MIT License.
-// // https://opensource.org/licenses/MIT
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+/* ===
+ml5 Example
+SketchRNN
+=== */
 
-import { useEffect } from "react"
-import * as ml5 from 'ml5';
-
-// /* ===
-// ml5 Example
-// SketchRNN
-// === */
-
-// import * as ml5 from 'ml5';
-// import { useRef, useEffect } from 'react';
-
-// let model;
-// // Start by drawing
-// let previousPen = "down";
-// // Current location of drawing
-// let pX = null;
-// let pY = null;
-// let x = null;
-// let y = null;
-// let mouseX = null;
-// let mouseY = null;
-// // The current "stroke" of the drawing
-// let strokePath;
-// let seedStrokes = [];
-// // Storing a reference to the canvas
-
-// const width = 640;
-// const height = 480;
-
-// let mouseDown = false;
-
-// export default function Drawzone() {
-//     const canvasRef = useRef(null)
-//     useEffect(() => {
-//         // The SketchRNN model
-//         const canvas = canvasRef.current;
-//         const ctx = canvas.getContext("2d");
-//         ctx.fillStyle = "#ebedef";
-//         ctx.fillRect(0, 0, width, height);
-//         (async () => {
-//             model = await ml5.sketchRNN("cat", () => canvas.addEventListener("mouseup", startSketchRNN));
-//             requestAnimationFrame(draw);
-//         })();
-//     }, [])
-//     function clearDrawing() {
-//         clearCanvas();
-//         // clear seed strokes
-//         seedStrokes = [];
-//         // Reset model
-//         model.reset();
-//     }
-//     function clearCanvas() {
-//         const canvas = canvasRef.current;
-//         const ctx = canvas.getContext("2d");
-//         ctx.fillStyle = "#ebedef";
-//         ctx.fillRect(0, 0, width, height);
-//     }
-//     return (
-//         <div>
-//             <button onClick={clearDrawing} type='button'>Clear</button>
-//             <canvas ref={canvasRef} height={640} width={480} onMouseMove={onMouseUpdate} onMouseDown={onMouseDown} onMouseUp={onMouseUp}></canvas>
-//         </div>
-//     );
-// }
+import Head from 'next/head';
+import { useRef, useEffect, useState } from 'react';
 
 
+// The SketchRNN model
+let model;
+// Start by drawing
+let previousPen = "down";
+// Current location of drawing
+let pX = null;
+let pY = null;
+let x = null;
+let y = null;
+let mouseX = null;
+let mouseY = null;
+// The current "stroke" of the drawing
+let strokePath;
+let seedStrokes = [];
+let button;
+// Storing a reference to the canvas
 
-// // The model is ready
+const width = 640;
+const height = 480;
+let canvas, ctx;
+let mouseDown = false;
+let currentModel = "cat";
+async function setup() {
+    ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#ebedef";
+    ctx.fillRect(0, 0, width, height);
+    // Load the model
+    // See a list of all supported models: https://github.com/ml5js/ml5-library/blob/master/src/SketchRNN/models.js
+    model = await ml5.sketchRNN(currentModel, modelReady);
 
-// // Reset the drawing
+    requestAnimationFrame(draw);
+}
+
+function modelReady() {
+    // sketchRNN will begin when the mouse is released
+    canvas.addEventListener("mouseup", startSketchRNN);
+}
+
+// Reset the drawing
+function clearDrawing() {
+    clearCanvas();
+    // clear seed strokes
+    seedStrokes = [];
+    // Reset model
+    model.reset();
+}
+
+// sketchRNN takes over
+function startSketchRNN() {
+    // Start where the mouse left off
+    x = mouseX;
+    y = mouseY;
+    // Generate with the seedStrokes
+    model.generate(seedStrokes, gotStroke);
+}
+
+const draw = ctx = () => {
+    requestAnimationFrame(draw);
+    if (pX == null || pY == null) {
+        pX = mouseX;
+        pY = mouseY;
+    }
+
+    if (mouseDown) {
+        // Set stroke weight to 10
+        ctx.lineWidth = 10;
+        // Set stroke color to black
+        ctx.strokeStyle = "#000000";
+        // If mouse is pressed, draw line between previous and current mouse positions
+        ctx.beginPath();
+        ctx.lineCap = "round";
+        ctx.moveTo(mouseX, mouseY);
+        ctx.lineTo(pX, pY);
+        ctx.stroke();
+
+        // Create a "stroke path" with dx, dy, and pen
+        const userStroke = {
+            dx: mouseX - pX,
+            dy: mouseY - pY,
+            pen: "down",
+        };
+        // Add to the array
+        seedStrokes.push(userStroke);
+    }
+
+    // If something new to draw
+    if (strokePath) {
+        // If the pen is down, draw a line
+        if (previousPen === "down") {
+            ctx.beginPath();
+            ctx.lineCap = "round";
+            ctx.moveTo(x, y);
+            ctx.lineTo(x + strokePath.dx, y + strokePath.dy);
+            ctx.stroke();
+        }
+        // Move the pen
+        x += strokePath.dx;
+        y += strokePath.dy;
+        // The pen state actually refers to the next stroke
+        previousPen = strokePath.pen;
+
+        // If the drawing is complete
+        if (strokePath.pen !== "end") {
+            strokePath = null;
+            model.generate(gotStroke);
+        }
+    }
+
+    // set the new pX and pY
+    pX = mouseX;
+    pY = mouseY;
+}
+
+// A new stroke path
+function gotStroke(err, s) {
+    strokePath = s;
+}
 
 
-// // sketchRNN takes over
-// function startSketchRNN() {
-//     // Start where the mouse left off
-//     x = mouseX;
-//     y = mouseY;
-//     // Generate with the seedStrokes
-//     model.generate(seedStrokes, gotStroke);
-// }
+function clearCanvas() {
+    ctx.fillStyle = "#ebedef";
+    ctx.fillRect(0, 0, width, height);
+}
 
-// function draw() {
-//     requestAnimationFrame(draw);
-//     if (pX == null || pY == null) {
-//         pX = mouseX;
-//         pY = mouseY;
-//     }
+function onMouseDown(e) {
+    mouseDown = true;
+}
 
-//     if (mouseDown) {
-//         // Set stroke weight to 10
-//         ctx.lineWidth = 10;
-//         // Set stroke color to black
-//         ctx.strokeStyle = "#000000";
-//         // If mouse is pressed, draw line between previous and current mouse positions
-//         ctx.beginPath();
-//         ctx.lineCap = "round";
-//         ctx.moveTo(mouseX, mouseY);
-//         ctx.lineTo(pX, pY);
-//         ctx.stroke();
+function onMouseUp(e) {
+    mouseDown = false;
+}
 
-//         // Create a "stroke path" with dx, dy, and pen
-//         const userStroke = {
-//             dx: mouseX - pX,
-//             dy: mouseY - pY,
-//             pen: "down",
-//         };
-//         // Add to the array
-//         seedStrokes.push(userStroke);
-//     }
+function onMouseUpdate(e) {
+    const pos = getMousePos(document.querySelector("canvas"), e);
+    mouseX = pos.x;
+    mouseY = pos.y;
+}
 
-//     // If something new to draw
-//     if (strokePath) {
-//         // If the pen is down, draw a line
-//         if (previousPen === "down") {
-//             ctx.beginPath();
-//             ctx.lineCap = "round";
-//             ctx.moveTo(x, y);
-//             ctx.lineTo(x + strokePath.dx, y + strokePath.dy);
-//             ctx.stroke();
-//         }
-//         // Move the pen
-//         x += strokePath.dx;
-//         y += strokePath.dy;
-//         // The pen state actually refers to the next stroke
-//         previousPen = strokePath.pen;
-
-//         // If the drawing is complete
-//         if (strokePath.pen !== "end") {
-//             strokePath = null;
-//             model.generate(gotStroke);
-//         }
-//     }
-
-//     // set the new pX and pY
-//     pX = mouseX;
-//     pY = mouseY;
-// }
-
-// // A new stroke path
-// function gotStroke(err, s) {
-//     strokePath = s;
-// }
-
-
-
-// function onMouseDown(e) {
-//     mouseDown = true;
-// }
-
-// function onMouseUp(e) {
-//     mouseDown = false;
-// }
-
-// function onMouseUpdate(e) {
-//     const pos = getMousePos(document.querySelector("canvas"), e);
-//     mouseX = pos.x;
-//     mouseY = pos.y;
-// }
-
-// function getMousePos(canvas, e) {
-//     const rect = canvas.getBoundingClientRect();
-//     return {
-//         x: e.clientX - rect.left,
-//         y: e.clientY - rect.top,
-//     };
-// }
+function getMousePos(canvas, e) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top,
+    };
+}
 
 export default function Drawzone() {
+    const canvasRef = useRef(null);
+    /* ===
+       ml5 Example
+       SketchRNN
+       === */
+
+    
     useEffect(() => {
         // Copyright (c) 2019 ml5
         //
         // This software is released under the MIT License.
         // https://opensource.org/licenses/MIT
-
-        /* ===
-        ml5 Example
-        SketchRNN
-        === */
-
-        // The SketchRNN model
-        let model;
-        // Start by drawing
-        let previousPen = "down";
-        // Current location of drawing
-        let pX = null;
-        let pY = null;
-        let x = null;
-        let y = null;
-        let mouseX = null;
-        let mouseY = null;
-        // The current "stroke" of the drawing
-        let strokePath;
-        let seedStrokes = [];
-        let button;
-        // Storing a reference to the canvas
-        let canvas, ctx;
-
-        const width = 640;
-        const height = 480;
-
-        let mouseDown = false;
-
-        async function setup() {
-            canvas = createCanvas(640, 480);
-            ctx = canvas.getContext("2d");
-            ctx.fillStyle = "#ebedef";
-            ctx.fillRect(0, 0, width, height);
-            // Load the model
-            // See a list of all supported models: https://github.com/ml5js/ml5-library/blob/master/src/SketchRNN/models.js
-            model = await ml5.sketchRNN("cat", modelReady);
-
-            // Button to start drawing
-            button = document.querySelector("#clearBtn");
-            button.addEventListener("click", clearDrawing);
-
-            canvas.addEventListener("mousemove", onMouseUpdate);
-            canvas.addEventListener("mousedown", onMouseDown);
-            canvas.addEventListener("mouseup", onMouseUp);
-
-            requestAnimationFrame(draw);
-        }
+        canvas = canvasRef.current;
+        ctx = canvas.getContext("2d")
         setup();
 
         // The model is ready
-        function modelReady() {
-            // sketchRNN will begin when the mouse is released
-            canvas.addEventListener("mouseup", startSketchRNN);
-        }
-
-        // Reset the drawing
-        function clearDrawing() {
-            clearCanvas();
-            // clear seed strokes
-            seedStrokes = [];
-            // Reset model
-            model.reset();
-        }
-
-        // sketchRNN takes over
-        function startSketchRNN() {
-            // Start where the mouse left off
-            x = mouseX;
-            y = mouseY;
-            // Generate with the seedStrokes
-            model.generate(seedStrokes, gotStroke);
-        }
-
-        function draw() {
-            requestAnimationFrame(draw);
-            if (pX == null || pY == null) {
-                pX = mouseX;
-                pY = mouseY;
-            }
-
-            if (mouseDown) {
-                // Set stroke weight to 10
-                ctx.lineWidth = 10;
-                // Set stroke color to black
-                ctx.strokeStyle = "#000000";
-                // If mouse is pressed, draw line between previous and current mouse positions
-                ctx.beginPath();
-                ctx.lineCap = "round";
-                ctx.moveTo(mouseX, mouseY);
-                ctx.lineTo(pX, pY);
-                ctx.stroke();
-
-                // Create a "stroke path" with dx, dy, and pen
-                const userStroke = {
-                    dx: mouseX - pX,
-                    dy: mouseY - pY,
-                    pen: "down",
-                };
-                // Add to the array
-                seedStrokes.push(userStroke);
-            }
-
-            // If something new to draw
-            if (strokePath) {
-                // If the pen is down, draw a line
-                if (previousPen === "down") {
-                    ctx.beginPath();
-                    ctx.lineCap = "round";
-                    ctx.moveTo(x, y);
-                    ctx.lineTo(x + strokePath.dx, y + strokePath.dy);
-                    ctx.stroke();
-                }
-                // Move the pen
-                x += strokePath.dx;
-                y += strokePath.dy;
-                // The pen state actually refers to the next stroke
-                previousPen = strokePath.pen;
-
-                // If the drawing is complete
-                if (strokePath.pen !== "end") {
-                    strokePath = null;
-                    model.generate(gotStroke);
-                }
-            }
-
-            // set the new pX and pY
-            pX = mouseX;
-            pY = mouseY;
-        }
-
-        // A new stroke path
-        function gotStroke(err, s) {
-            strokePath = s;
-        }
-
-        function createCanvas(w, h) {
-            const canvas = document.createElement("canvas");
-            canvas.width = w;
-            canvas.height = h;
-            document.body.appendChild(canvas);
-            return canvas;
-        }
-
-        function clearCanvas() {
-            ctx.fillStyle = "#ebedef";
-            ctx.fillRect(0, 0, width, height);
-        }
-
-        function onMouseDown(e) {
-            mouseDown = true;
-        }
-
-        function onMouseUp(e) {
-            mouseDown = false;
-        }
-
-        function onMouseUpdate(e) {
-            const pos = getMousePos(document.querySelector("canvas"), e);
-            mouseX = pos.x;
-            mouseY = pos.y;
-        }
-
-        function getMousePos(canvas, e) {
-            const rect = canvas.getBoundingClientRect();
-            return {
-                x: e.clientX - rect.left,
-                y: e.clientY - rect.top,
-            };
-        }
-    }, []);
+    });
     return (
-        <>
-            <h1>SketchRNN</h1>
-            <p><button id="clearBtn">Clear</button></p>
-        </>
-    )
+        <div>
+            <Head>
+                <script defer src="https://unpkg.com/ml5@latest/dist/ml5.min.js"></script>
+            </Head>
+            <h1>Start drawing a cat!</h1>
+            <canvas onMouseUp={onMouseUp} onMouseDown={onMouseDown} onMouseMove={onMouseUpdate} ref={canvasRef} height={640} width={480}></canvas>
+            <button onClick={clearDrawing} type='button'>Clear</button>
+        </div>
+    );
 }
+
