f27157f6357045e6a6ed45e01160f43dc252b2ce
Hien-Jason <101946880+Hien-Jason@users.noreply.github.com>
2022-03-19 16:55:24 -0400

    Update q1.jsx

--- pages/q1.jsx
+++ pages/q1.jsx
@@ -1,7 +1,16 @@
 export default function QuestionOne () {
     return (
         <div>
-            
+            <p>Let's make the computer read numbers</p>
+        
+            <form>  <!--asks how many digits there are? -->
+        
+                <label for="digits">First, how many digits are there? (Hint: count your fingers!)</label><br>
+                <p></p>
+                <input type="text" id="digits" name="digits"><br>
+                <p></p>
+                <input type="submit" value="Submit">    
+            </form>
         </div>
     )
 }
