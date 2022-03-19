e6bf36f1c5cf048fd51160639cee1096a5e61829
Hien-Jason <101946880+Hien-Jason@users.noreply.github.com>
2022-03-19 17:14:57 -0400

    Update q2.jsx

--- pages/q2.jsx
+++ pages/q2.jsx
@@ -1,7 +1,20 @@
 export default function QuestionTwo () {
     return (
         <div>
+            <p>Our numbers need to be in boxes with the same size</p>
+            <p>And We're going to use a 28x28 box for the numbers</p>
 
+            <form> <!-- Asks the height and width of the draw box -->        
+                <label for="height">can you tell the computer how tall the box will be? </label><br>
+                <p></p>
+                <input type="text" id="height" name="height"><br>
+                <p></p>
+                <label for="width">Then can you tell the computer how wide the box will be? </label><br>
+                <p></p>
+                <input type="text" id="width" name="width"><br>
+
+                <input type="submit" value="Submit">    
+            </form>
         </div>
     )
 }
